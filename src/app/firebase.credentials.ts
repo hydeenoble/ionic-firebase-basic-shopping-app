1892756821f6895a46fa1688406bed1c2da2e490
Emehinola Idowu <hydeenoble39@gmail.com>
2018-03-02 18:26:10 +0100

    Initialzing with firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+  apiKey: "AIzaSyC_JeVsHrPOn2zguK2fXV45LgmN2rQ41c0",
+  authDomain: "ionic-firabse-shopping-app.firebaseapp.com",
+  databaseURL: "https://ionic-firabse-shopping-app.firebaseio.com",
+  projectId: "ionic-firabse-shopping-app",
+  storageBucket: "ionic-firabse-shopping-app.appspot.com",
+  messagingSenderId: "859541569894"
+};
