13f3bf91f561b4b5587de4699c832419b99eee5e
Emehinola Idowu <hydeenoble39@gmail.com>
2018-03-03 11:56:34 +0100

    Added funtionality to add button

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AddShoppingPage} from "../add-shopping/add-shopping";
 
 /**
  * Generated class for the ShoppingListPage page.
@@ -18,8 +19,9 @@ export class ShoppingListPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  navigateToAddShoppingPage(): void {
+    this.navCtrl.push('AddShoppingPage')
   }
+
 
 }
